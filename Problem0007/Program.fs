8b956f45d1bee15c31af148c01ea513c980dfc4c
Avi Jassra <avijassra@hotmail.com>
2013-09-26 14:57:26 -0400

    solved problem 7

--- Problem0007/Program.fs
+++ Problem0007/Program.fs
@@ -7,5 +7,23 @@ What is the 10 001st prime number?
 *)
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let l = new ResizeArray<_>([])
+    let mutable num = 2I
+
+    let rec isPrime (x : bigint) (y : bigint) =
+        match x, y with
+            | _, _ when y > x/2I -> true
+            | _, _ when x%y = 0I -> false
+            | _, _ -> isPrime x (y+1I)
+
+    while l.Count < 10001 do
+        if isPrime num 2I then
+            printfn "Number added to list - %A" num
+            l.Add(num)
+        num <- num + 1I
+
+
+
+    printfn "10001st prime number is %A" l.[10000]
+    System.Console.Read() |> ignore
     0 // return an integer exit code

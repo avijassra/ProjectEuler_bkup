1b877d381a6244ee1752a9c6b4a64a211123ae7c
Avi Jassra <avijassra@hotmail.com>
2013-09-29 09:33:28 -0400

    Problem 10 updated

---
+++ Problem0009/Program.fs
@@ -0,0 +1,28 @@
+﻿(*
+Special Pythagorean triplet
+Problem 9
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*)
+
+[<EntryPoint>]
+let main argv = 
+    
+    let rec getPythagoreanTriplet a b c =
+        match a, b, c with
+            | x, y, z when x*x + y*y = z*z && x + y + z = 1000 -> x*y*z
+            | 1000, 1000, 1000 -> 0
+            | _, 1000, 1000 -> getPythagoreanTriplet (a+1) 1 1
+            | _, _, 1000 -> getPythagoreanTriplet a (b+1) 1
+            | _, _, _ -> getPythagoreanTriplet a b (c+1)
+
+    let product = getPythagoreanTriplet 20 20 20
+
+    printfn "%A" product
+    System.Console.Read() |> ignore
+    0 // return an integer exit code
---
+++ Problem0010/Program.fs
@@ -0,0 +1,31 @@
+﻿(*
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*)
+
+open System.Collections.Generic
+
+[<EntryPoint>]
+let main argv = 
+    let sieve (n:int64) =
+        let candidatePrimes = new Dictionary<int64, bool>()
+        for i in 2L .. n do candidatePrimes.Add(i, true)
+        for i in 2L .. n/2L do
+            if candidatePrimes.[i] = true then
+                let mutable j = i
+                while (j + i <= n) do
+                    j <- j + i
+                    candidatePrimes.[j] <- false
+        seq { for i in 2L .. n do
+                if candidatePrimes.[i] = true then
+                    yield i }
+        |> Seq.sum
+
+    let sumOfPrimeNumbers = sieve 2000000L
+
+    printfn "sum of all the primes below two million - %A" sumOfPrimeNumbers
+    System.Console.Read() |> ignore
+    0 // return an integer exit code

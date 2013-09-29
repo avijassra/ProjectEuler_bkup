438c51f24faa8a7c09f06d43b341e8a25f56c158
Avi Jassra <avijassra@hotmail.com>
2013-09-29 16:24:03 -0400

    updated with options

--- Problem0001/Program.fs
+++ Problem0001/Program.fs
@@ -8,10 +8,31 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 [<EntryPoint>]
 let main argv = 
-    let sumNo = [1..999]
-                    |> Seq.filter (fun x -> x % 3 = 0 || x % 5 = 0)
-                    |> Seq.sum
     
-    printfn "The sum of all the multiples of 3 or 5 below 1000 is %d" sumNo
+    // Option 1
+    let sumOfNoDivisbleBy3or5UnderN n = 
+        [for i in 1 .. n-1 -> i]
+            |> Seq.filter (fun x -> x%3 = 0 || x%5 = 0)
+            |> Seq.sum
+
+    let sumOfNoDivisbleBy3or5Under10 = sumOfNoDivisbleBy3or5UnderN 10
+
+    let sumOfNoDivisbleBy3or5Under1000 = sumOfNoDivisbleBy3or5UnderN 1000
+
+    printfn "The sum of all the multiples of 3 or 5 is %d" sumOfNoDivisbleBy3or5Under1000
+
+    // Option 2
+    let sumOfFilteredSeq filterCri n = 
+        seq { for i in 1 .. n-1 -> if filterCri i then i else 0 }
+            |> Seq.sum
+
+    let filteredSeqOf3or5 = sumOfFilteredSeq (fun x -> x%3 = 0 || x%5 = 0)
+
+    let sumOfNoUnder10 = filteredSeqOf3or5 10
+
+    let sumOfNoUnder1000 = filteredSeqOf3or5 1000
+
+    printfn "The sum of all the multiples of 3 or 5 is %d" sumOfNoUnder1000
+
     System.Console.Read() |> ignore
     0 // return an integer exit code

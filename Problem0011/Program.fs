aca65abd8b6cc212e8955edbd9a0408b7509f634
Avi Jassra <avijassra@hotmail.com>
2013-11-04 14:32:59 -0500

    Problem 11 solved
    Finally

--- Problem0011/Program.fs
+++ Problem0011/Program.fs
@@ -30,72 +30,76 @@ What is the greatest product of four adjacent numbers in the same direction (up,
 
 [<EntryPoint>]
 let main argv = 
-    let matrix =    [|
-                        [|08; 02; 22; 97; 38; 15; 00; 40; 00; 75; 04; 05; 07; 78; 52; 12; 50; 77; 91; 08;|];
-                        [|49; 49; 99; 40; 17; 81; 18; 57; 60; 87; 17; 40; 98; 43; 69; 48; 04; 56; 62; 00;|];
-                        [|81; 49; 31; 73; 55; 79; 14; 29; 93; 71; 40; 67; 53; 88; 30; 03; 49; 13; 36; 65;|];
-                        [|52; 70; 95; 23; 04; 60; 11; 42; 69; 24; 68; 56; 01; 32; 56; 71; 37; 02; 36; 91;|];
-                        [|22; 31; 16; 71; 51; 67; 63; 89; 41; 92; 36; 54; 22; 40; 40; 28; 66; 33; 13; 80;|];
-                        [|24; 47; 32; 60; 99; 03; 45; 02; 44; 75; 33; 53; 78; 36; 84; 20; 35; 17; 12; 50;|];
-                        [|32; 98; 81; 28; 64; 23; 67; 10; 26; 38; 40; 67; 59; 54; 70; 66; 18; 38; 64; 70;|];
-                        [|67; 26; 20; 68; 02; 62; 12; 20; 95; 63; 94; 39; 63; 08; 40; 91; 66; 49; 94; 21;|];
-                        [|24; 55; 58; 05; 66; 73; 99; 26; 97; 17; 78; 78; 96; 83; 14; 88; 34; 89; 63; 72;|];
-                        [|21; 36; 23; 09; 75; 00; 76; 44; 20; 45; 35; 14; 00; 61; 33; 97; 34; 31; 33; 95;|];
-                        [|78; 17; 53; 28; 22; 75; 31; 67; 15; 94; 03; 80; 04; 62; 16; 14; 09; 53; 56; 92;|];
-                        [|16; 39; 05; 42; 96; 35; 31; 47; 55; 58; 88; 24; 00; 17; 54; 24; 36; 29; 85; 57;|];
-                        [|86; 56; 00; 48; 35; 71; 89; 07; 05; 44; 44; 37; 44; 60; 21; 58; 51; 54; 17; 58;|];
-                        [|19; 80; 81; 68; 05; 94; 47; 69; 28; 73; 92; 13; 86; 52; 17; 77; 04; 89; 55; 40;|];
-                        [|04; 52; 08; 83; 97; 35; 99; 16; 07; 97; 57; 32; 16; 26; 26; 79; 33; 27; 98; 66;|];
-                        [|88; 36; 68; 87; 57; 62; 20; 72; 03; 46; 33; 67; 46; 55; 12; 32; 63; 93; 53; 69;|];
-                        [|04; 42; 16; 73; 38; 25; 39; 11; 24; 94; 72; 18; 08; 46; 29; 32; 40; 62; 76; 36;|];
-                        [|20; 69; 36; 41; 72; 30; 23; 88; 34; 62; 99; 69; 82; 67; 59; 85; 74; 04; 36; 16;|];
-                        [|20; 73; 35; 29; 78; 31; 90; 01; 74; 31; 49; 71; 48; 86; 81; 16; 23; 57; 05; 54;|];
-                        [|01; 70; 54; 71; 83; 51; 54; 69; 16; 92; 33; 48; 61; 43; 52; 01; 89; 19; 67; 48;|];
-                    |]
+    let matrix =    [
+                        [08; 02; 22; 97; 38; 15; 00; 40; 00; 75; 04; 05; 07; 78; 52; 12; 50; 77; 91; 08;];
+                        [49; 49; 99; 40; 17; 81; 18; 57; 60; 87; 17; 40; 98; 43; 69; 48; 04; 56; 62; 00;];
+                        [81; 49; 31; 73; 55; 79; 14; 29; 93; 71; 40; 67; 53; 88; 30; 03; 49; 13; 36; 65;];
+                        [52; 70; 95; 23; 04; 60; 11; 42; 69; 24; 68; 56; 01; 32; 56; 71; 37; 02; 36; 91;];
+                        [22; 31; 16; 71; 51; 67; 63; 89; 41; 92; 36; 54; 22; 40; 40; 28; 66; 33; 13; 80;];
+                        [24; 47; 32; 60; 99; 03; 45; 02; 44; 75; 33; 53; 78; 36; 84; 20; 35; 17; 12; 50;];
+                        [32; 98; 81; 28; 64; 23; 67; 10; 26; 38; 40; 67; 59; 54; 70; 66; 18; 38; 64; 70;];
+                        [67; 26; 20; 68; 02; 62; 12; 20; 95; 63; 94; 39; 63; 08; 40; 91; 66; 49; 94; 21;];
+                        [24; 55; 58; 05; 66; 73; 99; 26; 97; 17; 78; 78; 96; 83; 14; 88; 34; 89; 63; 72;];
+                        [21; 36; 23; 09; 75; 00; 76; 44; 20; 45; 35; 14; 00; 61; 33; 97; 34; 31; 33; 95;];
+                        [78; 17; 53; 28; 22; 75; 31; 67; 15; 94; 03; 80; 04; 62; 16; 14; 09; 53; 56; 92;];
+                        [16; 39; 05; 42; 96; 35; 31; 47; 55; 58; 88; 24; 00; 17; 54; 24; 36; 29; 85; 57;];
+                        [86; 56; 00; 48; 35; 71; 89; 07; 05; 44; 44; 37; 44; 60; 21; 58; 51; 54; 17; 58;];
+                        [19; 80; 81; 68; 05; 94; 47; 69; 28; 73; 92; 13; 86; 52; 17; 77; 04; 89; 55; 40;];
+                        [04; 52; 08; 83; 97; 35; 99; 16; 07; 97; 57; 32; 16; 26; 26; 79; 33; 27; 98; 66;];
+                        [88; 36; 68; 87; 57; 62; 20; 72; 03; 46; 33; 67; 46; 55; 12; 32; 63; 93; 53; 69;];
+                        [04; 42; 16; 73; 38; 25; 39; 11; 24; 94; 72; 18; 08; 46; 29; 32; 40; 62; 76; 36;];
+                        [20; 69; 36; 41; 72; 30; 23; 88; 34; 62; 99; 69; 82; 67; 59; 85; 74; 04; 36; 16;];
+                        [20; 73; 35; 29; 78; 31; 90; 01; 74; 31; 49; 71; 48; 86; 81; 16; 23; 57; 05; 54;];
+                        [01; 70; 54; 71; 83; 51; 54; 69; 16; 92; 33; 48; 61; 43; 52; 01; 89; 19; 67; 48;];
+                    ]
 
-    let checkGreaterProduct prod1 prod2 =
-        if prod1 > prod2 then
-            prod1
+    let calcGrtNo prod n1 n2 n3 n4 =
+        let newNum = (n1 * n2 * n3 * n4)
+
+        if prod > newNum then
+            prod
         else
-            prod2
+            newNum
 
-    let horizontalProduct prod (l: int list) = 
-        match l with
-            | a::(b::c::d::_ as t) -> checkGreaterProduct prod (a*b*c*d)
-            | _ -> prod
+    let rec calcOnlyRow prod = function
+        | h::(t1::t2::t3::_ as t) ->
+            calcOnlyRow (calcGrtNo prod h t1 t2 t3) t
+        | [_;_;_] | [_;_] | [_] | [] -> prod
 
-    let verticalProduct prod (row1: int list) (row2: int list) (row3: int list) (row4: int list) index = 
-        checkGreaterProduct prod (row1.[index] * row2.[index] * row3.[index] * row4.[index])
+    let rec calcOnlyCol prod r1 r2 r3 r4 = 
+        match r1, r2, r3, r4 with
+            | r1h::r1t, r2h::r2t, r3h::r3t, r4h::r4t ->
+                calcOnlyCol (calcGrtNo prod r1h r2h r3h r3h) r1t r2t r3t r4t
+            | _,_,_,_ -> prod
 
-    let diagnoalFwdProduct prod (row1: int list) (row2: int list) (row3: int list) (row4: int list) index = 
-        match row1 with
-            | a::(b::c::d::_ as t) -> checkGreaterProduct prod (row1.[index] * row2.[index+1] * row3.[index+2] * row4.[index+3])
-            | _ -> prod
+    let rec calcAllProds prod r1 r2 r3 r4 = 
+        match r1, r2, r3, r4 with
+            | ([x;y]), ([u;v]), ([m;n]), ([t;s]) ->
+                let verProd1 = calcGrtNo prod x u m t
+                calcGrtNo verProd1 y v n s
+            | (r1h::(r1t1::r1t2::r1t3::_ as r1t)), (r2h::(r2t1::r2t2::r2t3::_ as r2t)), (r3h::(r3t1::r3t2::r3t3::_ as r3t)), (r4h::(r4t1::r4t2::r4t3::_ as r4t)) ->
+                let horProd = calcGrtNo prod r1h r1t1 r1t2 r1t3
+                let verProd = calcGrtNo horProd r1h r2h r3h r4h
+                let diaProd1 = calcGrtNo verProd r1h r2t1 r3t2 r4t3
+                let diaProd2 = calcGrtNo diaProd1 r1t3 r2t2 r3t1 r4h
+                calcAllProds diaProd2 r1t r2t r3t r4t
+            | (r1h::(r1t1::r1t2 as r1t)), (r2h::(r2t1::r2t2 as r2t)), (r3h::(r3t1::r3t2 as r3t)), (r4h::(r4t1::r4t2 as r4t)) ->
+                let verProd = calcGrtNo prod r1h r2h r3h r4h
+                calcAllProds verProd r1t r2t r3t r4t
+            | _,_,_,_ -> prod
 
-    let diagnoalBckProduct prod (row1: int list) (row2: int list) (row3: int list) (row4: int list) index = 
-        match row4 with
-            | a::(b::c::d::_ as t) -> checkGreaterProduct prod (row1.[index+3] * row2.[index+2] * row3.[index+1] * row4.[index])
-            | _ -> prod
+    let rec matrixCheck prod matrix =
+        match matrix with
+            | h::(t1::t2::t3::_ as t) ->
+                let newProd = calcAllProds prod h t1 t2 t3
+                matrixCheck newProd t
+            | h::t ->
+                let newProd = calcOnlyRow prod h
+                matrixCheck newProd t
+            | [] -> prod
+
+    let greatestProdInMatrix = matrixCheck 0 matrix
     
-    let calcMaxProdOfPoint prod row1 row2 row3 row4 index =
-        let hProd = horizontalProduct prod row1
-        let vProd = verticalProduct hProd row1 row2 row3 row4 index
-        let dfProd = diagnoalFwdProduct vProd row1 row2 row3 row4 index
-        let dbProd = diagnoalBckProduct dfProd row1 row2 row3 row4 index
-        dbProd
-
-    let rec getProduct (prod: int) = function
-        | (a: int list)::(b::c::d::_ as t) -> getProduct (a |> Seq.mapi (fun i v -> match a with 
-                                                                                        | x::(y::z::u::_ as t) -> calcMaxProdOfPoint prod a b c d i
-                                                                                        | _ -> prod) |> Seq.max) t
-        | h::t -> getProduct (horizontalProduct prod h) t
-        | _ -> prod
-
-//    let maxProd = getProduct 0 matrix
-//
-//    printfn "%A" maxProd
-
-
-    
+    printfn "%A" greatestProdInMatrix
     System.Console.Read() |> ignore
     0 // return an integer exit code

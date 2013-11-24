67d4c36f313a37cf9581c209fc840c7b4f65f6f6
Avi Jassra <avijassra@hotmail.com>
2013-11-24 08:26:47 -0500

    updated with comments and diagnostic stopwatch

--- Problem0003/Program.fs
+++ Problem0003/Program.fs
@@ -10,20 +10,77 @@ What is the largest prime factor of the number 600851475143 ?
 let main argv =
     let theNumber = 600851475143I //13195
 
-    let rec LargestPrimeFactorial (x: bigint) (y:bigint) = 
-        match x, y with
-            | u, t when u = t -> u
-            | c, d when c%d = 0I -> 
-                match c with
-                    | m when m = theNumber -> LargestPrimeFactorial (m/d) 2I
-                    | n -> LargestPrimeFactorial theNumber ((theNumber/n)+1I)
-            | e, f -> LargestPrimeFactorial e (f+1I)
-            | a, b when a = b -> a
-            | _, _ -> failwith "not valid option"
+    (**********************************
+    Solution 1 
+    ***********************************
+    slow solution as it iterates through all the numbers
+    **********************************)
 
-    let primeFactorNo = LargestPrimeFactorial theNumber 2I
+//    let rec LargestPrimeFactorial (x: bigint) (y:bigint) = 
+//        match x, y with
+//            | u, t when u = t -> u
+//            | c, d when c%d = 0I -> 
+//                match c with
+//                    | m when m = theNumber -> LargestPrimeFactorial (m/d) 2I
+//                    | n -> LargestPrimeFactorial theNumber ((theNumber/n)+1I)
+//            | e, f -> LargestPrimeFactorial e (f+1I)
+//            | a, b when a = b -> a
+//            | _, _ -> failwith "not valid option"
+//
+//    let sw = System.Diagnostics.Stopwatch()
+//    sw.Start()  
+//    let primeFactorNo = LargestPrimeFactorial theNumber 2I
+//    sw.Stop()
 
-    printfn "%A" primeFactorNo 
+    (**********************************
+    Solution 2
+    ***********************************
+    Better solution to find all the divisors
+    if n / x = y, obviously both x and y are factors, so I don't have to probe y anymore
+    If I start probing from 2 going up, I can stop probing when I have reached \sqrt n. In fact, any number bigger than \sqrt n must have been already found probing a smaller number.
+    Suppose for example that we have to factor the number 36 (one of the triangle number)
+    **********************************)
+    let divides (x:bigint) (y:bigint) = (x % y = 0I)
+
+    let rec GetAllFactors (num:bigint) (index:bigint) factors = 
+        if (divides num index) then
+            let y = num / index
+            if index < y then
+                GetAllFactors num (index + 1I) (index::y::factors)
+            else if (y = index) then
+                (index::factors)
+            else 
+                factors
+        else if index > bigint (sqrt (float num)) then
+            factors
+        else 
+            GetAllFactors num (index + 1I) factors
+
+    let rec PrimeNumber = function
+        | [] -> 0
+        | h::t -> 
+            let factors = GetAllFactors h 2I []
+            if(factors.Length = 0) then
+                int h
+            else 
+                PrimeNumber t
+
+    let rec LargestPrimeFactorial (x: bigint) = 
+        let factorList = GetAllFactors x 2I []
+        let factorRevList = factorList
+                                |> List.sort 
+                                |> List.rev
+
+        let largestPrimeNumber = PrimeNumber factorRevList
+        largestPrimeNumber        
+    
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()    
+    let primeFactorNo = LargestPrimeFactorial theNumber
+    sw.Stop()
+
+
+    printfn "%A - (with processing time %A)" primeFactorNo  sw.ElapsedMilliseconds
 
     System.Console.Read() |> ignore
     0 // return an integer exit code
--- Problem0004/Program.fs
+++ Problem0004/Program.fs
@@ -9,28 +9,57 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 open System
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
+
+    // function to flip the existing number to reverse the digits 
     let reverseNumber n =
-        let rec loop acc = function
-            |0 -> acc
-            |x -> loop (acc * 10 + x % 10) (x/10)    
+        // recurring function to create new number to flip the existing the number digit one at a time
+        let rec loop newNum = function
+            // if the new number is 0
+            // return the new created number as it is
+            |0 -> newNum
+            // if number is not 0
+            // recall the function with new number * 10 and adding modules of number dividing by 10.
+            // return update old number by dividing by 10 
+            |x -> loop (newNum * 10 + x % 10) (x/10)    
         loop 0 n
 
+    // check if the number is palindrome
     let isPalindrome = function
+        // if number is equal to reversed number then its palindrome, return truw
         | x  when x = reverseNumber x -> true
+        // else return false
         | _ -> false
 
-
-    let maxPalindromeNumber =
+    // function to get the max palindrome number of multiplication of two numbers between the limits
+    let maxPalindromeNumber lowerLimit upperLimit =
+        // create the seq of all the palidrome numbers create by multiple two numbers between the limits
         let numbers = seq {
-            for i = 100 to 999 do
-                for j = 100 to 999 do
+            // get first number from the loop
+            for i = lowerLimit to upperLimit do
+                // get the second number from the loop
+                for j = lowerLimit to upperLimit do
+                    // multiple both the numeber
                     let z = i * j
+                    // and check if number is palindrome
                     if isPalindrome z then
+                        // if yes, return the number to seq else move back to new numbers in the loop
                         yield z
         }
+        // return the max number from the seq
         Seq.max numbers
 
-    printfn "multple of 3 digit resulting %d" maxPalindromeNumber
+    // create diagnostics stopwatch
+    let sw = System.Diagnostics.Stopwatch()
+    // start the stopwatch
+    sw.Start() 
+    // call function to get max palindrome number product of two numbers between 100 and 900
+    let number = maxPalindromeNumber 100 999
+    // stop the stopwatch
+    sw.Stop()
+    
+    // print number
+    printfn "multple of 3 digit resulting %d  - (with processing time %A)" number sw.ElapsedMilliseconds
+    
     Console.Read() |> ignore
     0 // return an integer exit code

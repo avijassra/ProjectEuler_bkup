f1c3d090065a12fd92527fe0088aec30ab24d920
Avi Jassra <avijassra@hotmail.com>
2014-05-19 17:30:30 -0400

    option 1 for c# 004 done ... bit slow

--- CSharp/Problem0004/Program.cs
+++ CSharp/Problem0004/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Problem0004
 {
@@ -10,6 +11,49 @@ namespace Problem0004
     {
         static void Main(string[] args)
         {
+            // OPTION 1
+            var sw = new Stopwatch();
+            sw.Start();
+            var maxProduct = ListOfThreeDigitNumbersProduct().Max();
+            sw.Stop();
+
+            ProjectEuler.Problem.AddResult(maxProduct, sw.ElapsedMilliseconds, "looping thru all 3 digits number and their product, short list Palindrome numbers and find max in them");
+            ProjectEuler.Problem.PrintResult("0004");
+        }
+
+        public static IEnumerable<int> ListOfThreeDigitNumbersProduct()
+        {
+            for (var i = 999; i > 99; i--)
+            {
+                for (var j = 999; j > 99; j--)
+                {
+                    var product = i*j;
+
+                    if (product.CheckPalindrome())
+                    {
+                        yield return product;
+                    }
+                }
+            }
         }
     }
+
+    public static class Palindrome
+    {
+        public static bool CheckPalindrome(this int number)
+        {
+            var actualNumber = number;
+            var palindromeNumber = 0;
+
+            while (number != 0)
+            {
+                palindromeNumber = (palindromeNumber * 10) + (number % 10);
+                number = number / 10;
+            }
+
+            return (actualNumber == palindromeNumber);
+        }
+    }
+
 }
+
